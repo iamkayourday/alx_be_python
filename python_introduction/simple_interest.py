bd9f92d20096ffa75d1f4c08b068be17744a2834
Abdulbasit <abdulbasitkayode@gmail.com>
2025-07-07 13:57:59 +0100

    Add simple interest calculator script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# Simple Interest Calculator
+# This script calculates the simple interest.
+# Formula: Simple Interest = (Principal * Rate * Time) / 100 (I = P*R*T)
+
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = (principal * rate * time)
+print(f"The Simple Interest is: {interest}")
+
+# Output: The Simple Interest is: 150.0
