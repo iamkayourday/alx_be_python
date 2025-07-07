a285e49a3cfef6200fe24b7c91574aec60aff22a
Abdulbasit <abdulbasitkayode@gmail.com>
2025-07-07 13:48:33 +0100

    Add basic arithmetic operations  in basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,20 @@
+# Basic Arithmetic Exercise
+
+number1 = 10
+number2 = 5
+
+# Perform addition
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum}")
+
+# perform subtraction
+difference = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference}")
+
+# Perform Multiplication
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product}")
+
+# Perform Division
+division = number1 / number2
+print(f"Division of {number1} by {number2} is {division}")
