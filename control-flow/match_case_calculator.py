89ecb3f10c897056fe271dd2327efbd5c69b1317
Abdulbasit <abdulbasitkayode@gmail.com>
2025-07-14 20:41:48 +0100

    Fix variable name for operation selection in match-case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,9 +6,9 @@ print(f"Welcome to the simple calculator!!!")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-operations = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operations:
+match operation:
     case '+':
         result = num1 + num2
         print(f"The result is {result}")
